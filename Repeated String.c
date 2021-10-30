e5f1a17295c6627f30fa042a5232eb84bbcf7ed8
Ranjul_Arumadi <54582161+Ranjul-Arumadi@users.noreply.github.com>
2021-10-30 11:57:30 +0530

    Update Repeated String.c

--- Repeated String.c
+++ Repeated String.c
@@ -28,3 +28,46 @@ int main()
     repeatedString("abcac", 10);
     return 0;
 }
+/*--------------------------------------BETTER METHOD-----------------------------------------*/
+#include <stdio.h>
+#include<string.h>
+int repeatedString(char* s, int n) {
+    
+    //calc. nnumber of times 'a' comes in original string
+    int aFrequency,aFrequencyRemaining,aTotalFrequency;
+    
+    int stringLength = strlen(s);
+    
+    for(int i=0;i<stringLength;i++){
+        if(s[i]=='a'){
+            aFrequency++;
+        }
+    }
+
+    //find how many times the original string will repeat
+    int repeatFrequency = n/stringLength;
+    if(aFrequency%2==0){
+        return aFrequency*repeatFrequency;
+    }
+    else{
+        //find where the string will end when incomplete
+        int stringEndPosition = n - (repeatFrequency*stringLength);
+        for(int i=0;i<stringEndPosition;i++){
+        
+            if(s[i]=='a'){
+                aFrequencyRemaining++;
+            }
+        }
+    }
+    
+    aTotalFrequency = (aFrequency*repeatFrequency) + aFrequencyRemaining;
+    return aTotalFrequency;
+}
+
+int main()
+{
+    //Enter string and the number of times it should repeat
+    int x = repeatedString("abcac", 10);
+    printf("Ans = %d",x);
+    return 0;
+}
