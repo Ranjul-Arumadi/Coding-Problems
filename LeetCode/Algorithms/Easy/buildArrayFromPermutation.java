05504c894af45d5b5e830c781eb4118b8c76bbff
Ranjul Arumadi <54582161+Ranjul-Arumadi@users.noreply.github.com>
2022-08-28 14:56:51 +0530

    Create buildArrayFromPermutation.java

---
+++ LeetCode/Algorithms/Easy/buildArrayFromPermutation.java
@@ -0,0 +1,31 @@
+/*
+	link: https://leetcode.com/problems/build-array-from-permutation/
+	
+	Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+
+	A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+
+	 
+
+	Example 1:
+
+	Input: nums = [0,2,1,5,3,4]
+	Output: [0,1,2,4,5,3]
+	Explanation: The array ans is built as follows: 
+	ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
+		= [nums[0], nums[2], nums[1], nums[5], nums[3], nums[4]]
+		= [0,1,2,4,5,3]
+*/
+
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int l = nums.length;
+        int ans[] = new int[l];
+        
+        for(int i=0;i<l;i++){
+            ans[i] = nums[nums[i]];
+        }
+        
+        return ans;
+    }
+}
