9fe96e72da148d2f63db90dbe8eceee03ceeff61
Ranjul Arumadi <54582161+Ranjul-Arumadi@users.noreply.github.com>
2022-08-29 07:02:38 +0530

    Update AddTwoIntegers.java

--- LeetCode/Algorithms/Easy/AddTwoIntegers.java
+++ LeetCode/Algorithms/Easy/AddTwoIntegers.java
@@ -1,3 +1,16 @@
+/*
+    link: https://leetcode.com/problems/add-two-integers/
+    
+    Given two integers num1 and num2, return the sum of the two integers.
+ 
+
+    Example 1:
+
+    Input: num1 = 12, num2 = 5
+    Output: 17
+    Explanation: num1 is 12, num2 is 5, and their sum is 12 + 5 = 17, so 17 is returned.
+*/
+
 class Solution {
     public int sum(int num1, int num2) {
         return num1+num2;
