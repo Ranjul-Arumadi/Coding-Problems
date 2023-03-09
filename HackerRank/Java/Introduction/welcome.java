3449a24dd8442c55c2c8bc40dfbe481ac3b4580a
Ranjul Arumadi <54582161+Ranjul-Arumadi@users.noreply.github.com>
2023-03-09 19:10:56 +0530

    Create welcome.java

---
+++ HackerRank/Java/Introduction/welcome.java
@@ -0,0 +1,12 @@
+/*
+Question: https://www.hackerrank.com/challenges/welcome-to-java/problem?isFullScreen=true
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
