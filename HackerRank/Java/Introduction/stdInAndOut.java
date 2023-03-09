466f70b8399c1cf3d351cc8b0699a94e903e3554
Ranjul Arumadi <54582161+Ranjul-Arumadi@users.noreply.github.com>
2023-03-09 19:23:04 +0530

    Create stdInAndOut.java

---
+++ HackerRank/Java/Introduction/stdInAndOut.java
@@ -0,0 +1,23 @@
+/*
+Question: https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem?isFullScreen=true
+*/
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        // Complete this line
+        // Complete this line
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        // Complete this line
+        // Complete this line
+    }
+}
